7d417ad8514505e425e09f0a1b7b4c96f5eab25c
mariiamdiian <mariiam.diiab@gmail.com>
2025-04-18 15:08:43 +0200

    feat: attach screenshots to Allure reports for failed tests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,6 @@
 package tests;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import utilites.BrowserOptions;
